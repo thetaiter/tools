0938f6a3c190856bee40281811441d342ea7d719
Ian Tait <thetaiter@gmail.com>
2022-08-14 20:27:29 -0500

    Added tools for internet archive

---
+++ internetarchive/PS2/download_sequential.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+
+PID="${$}"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+SCRIPT_NAME="$(basename "$(test -L "${0}" && readlink "${0}" || echo "${0}")")"
+
+declare -a IDENTIFIER_LIST=( $(cat "${1:-./identifiers.txt}") )
+INCLUDE_BLOB='*USA*.zip|*En,*.zip'
+
+function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
+
+for identifier in "${IDENTIFIER_LIST[@]}"
+do
+    # Generate ewxclude blob
+    EXCLUDE_BLOB='* Demo *|*(Proto)*|*Demo Disk*|*Demo 1*|*Demo 2*|*Bonus Demo*|*(Beta)*|*Demo Disc*|*Demo)*|*Beta 1*|*Beta 2*|*Beta 3*'
+    FILES=( $(ia download --search "identifier:${identifier}" --glob "${INCLUDE_BLOB}" --exclude "${EXCLUDE_BLOB}" --no-directories --dry-run) )
+
+    oIFS="${IFS}"
+    IFS=$'\n'
+    PROPER_FILES=( $(
+        for file in "${FILES[@]}"
+        do
+            urldecode "${file}"
+        done | sort -u
+    ) )
+
+    USA_FILES=( $(
+        for file in "${PROPER_FILES[@]}"
+        do
+            echo "${file}"
+        done | grep '(USA)'
+    ) )
+
+    NON_USA_FILES=( $(
+        for file in "${PROPER_FILES[@]}"
+        do
+            echo "${file}"
+        done | grep -v '(USA)'
+    ) )
+    IFS="${oIFS}"
+
+    for file in "${NON_USA_FILES[@]}"
+    do
+        FILE_NAME="$(echo "${file}" | cut -d\/ -f6)"
+        GAME_NAME="$(echo "${FILE_NAME}" | cut -d\( -f1 | awk '{$1=$1};1')"
+        if [[ " ${USA_FILES[@]} " =~ "/${GAME_NAME} (USA)" ]]
+        then
+            EXCLUDE_BLOB="${EXCLUDE_BLOB}|${FILE_NAME}"
+        fi
+    done
+
+    for file in "${PROPER_FILES[@]}"
+    do
+        FILE_NAME="$(echo "${file}" | cut -d\/ -f6)"
+        GAME_NAME="$(echo "${FILE_NAME}" | cut -d\( -f1 | awk '{$1=$1};1')"
+        if [ -f "${FILE_NAME}" ] || [ -f "${FILE_NAME/.zip/.tar.gz}" ]
+        then
+            EXCLUDE_BLOB="${EXCLUDE_BLOB}|${FILE_NAME}"
+        fi
+    done
+
+    echo "Downloading files from identifier ${identifier}"
+    ia download --search "identifier:${identifier}" --glob "${INCLUDE_BLOB}" --exclude "${EXCLUDE_BLOB}" --no-directories
+    read -p "Finished downloading files from ${identifier}. Press enter to continue..."
+done
---
+++ internetarchive/PS2/fix_roms.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+
+PID="${$}"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+SCRIPT_NAME="$(basename "$(test -L "${0}" && readlink "${0}" || echo "${0}")")"
+
+if compgen -G "*.zip" > /dev/null
+then
+    for file in *.zip
+    do
+        echo "Processing ${file}"
+
+        name="${file%.zip}"
+        game="$(echo "${file}" | cut -d\( -f 1 | awk '{$1=$1};1')"
+
+        if [ -f "${name}.tar.gz" ]
+        then
+            echo "Completed file already exists at '${name}.tar.gz'"
+            rm -fv "${file}"
+            echo "Finished processing '${file}'"
+            continue
+        fi
+
+        if [[ "${file}" != "${game} (USA)"* ]] && compgen -G "${game} (USA)*" > /dev/null
+        then
+            echo "USA Version already exists at '$(ls "${game} (USA)"*)'"
+            rm -fv "${file}"
+            echo "Finished processing '${file}'"
+            continue
+        fi
+
+        echo "Unzipping file '${file}'"
+        unzip "${file}"
+        echo "Unzip complete"
+
+        if compgen -G "*.cue" > /dev/null
+        then
+            rm -fv *.cue
+        fi
+
+        if compgen -G "*.bin" > /dev/null
+        then
+            for bin in *.bin
+            do
+                mv -fv "${bin}" "${bin/.bin/.iso}"
+            done
+        fi
+
+        if compgen -G "*.iso" > /dev/null
+        then
+            for iso in  *.iso
+            do
+                echo "Compressing file '${iso}'"
+                tar -cf - "${iso}" | pigz -p 3 > "${iso%.iso}.tar.gz"
+                echo "Compressed to '${iso%.iso}.tar.gz'"
+            done
+        fi
+
+        rm -fv *.iso "${file}"
+
+        echo "Finished processing '${file}'"
+    done
+else
+    echo "No zip files found."
+fi
